0a5a6bd882c49cf0389cec7e47cf5a765525ea71
nxa391 <nxa391@student.bham.ac.uk>
2024-11-18 16:52:49 +0000

    added bulk information endpoint for initial page to pull wider info about each returned recipe

--- Main/API/recipeList Page.py
+++ Main/API/recipeList Page.py
@@ -1,10 +1,12 @@
 import http.client
+import json
+
 
 #Replace this with data pulled from database, using test data for now
 ingredients = ["apples","bananas","milk","butter","chicken","steak","carrots","yoghurt","ketchup"]
 
 
-conn = http.client.HTTPSConnection("spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+conn1 = http.client.HTTPSConnection("spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
 
 headers = {
     'x-rapidapi-key': "855ba63510msh87c18edc980da90p1a2842jsncc58ceeb5d6a",
@@ -15,12 +17,38 @@ requestUrl = "/recipes/findByIngredients?ingredients="
 for i in ingredients:
     requestUrl+=i+"%2C"
 
-requestUrl = requestUrl[:-3]+"number=10"
-conn.request("GET", requestUrl, headers=headers)
+requestUrl = requestUrl[:-3]+"number=2"
+conn1.request("GET", requestUrl, headers=headers)
 
-res = conn.getresponse()
+res = conn1.getresponse()
 data = res.read()
 
 ingredientSearchResult = data.decode("utf-8")
-print(ingredientSearchResult)
+ingredientSearchResultJSON = json.loads(ingredientSearchResult)
+#print(type(ingredientSearchResult))
 
+
+conn2 = http.client.HTTPSConnection("spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "855ba63510msh87c18edc980da90p1a2842jsncc58ceeb5d6a",
+    'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+}
+
+
+requestUrl = "/recipes/informationBulk?ids="
+
+for i in ingredientSearchResultJSON:
+    print(i)
+    
+    requestUrl+=str(i["id"])+"%2C"
+
+requestUrl=requestUrl[:-3]
+
+conn2.request("GET", requestUrl, headers=headers)
+
+res = conn2.getresponse()
+data = res.read()
+
+bulkRecipeInfoJSON = json.loads(data.decode("utf-8"))
+#print(bulkRecipeInfoJSON)
